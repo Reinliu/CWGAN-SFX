1b0ca19f1d9b7a5a0060ca45f3cee105dc12ffa0
Rein Liu <50271800+Reinliu@users.noreply.github.com>
2023-02-09 17:34:04 +0800

    Update train_cctifgan.py

--- Models/Baseline/train_cctifgan.py
+++ Models/Baseline/train_cctifgan.py
@@ -1,12 +1,6 @@
-# Module to download the dataset.
+# Import the required libraries.
 import librosa
 import numpy as np
-from ourLTFATStft import LTFATStft
-import ltfatpy
-from modGabPhaseGrad import modgabphasegrad
-ltfatpy.gabphasegrad = modgabphasegrad # This function is not implemented for one sided stfts with the phase method on ltfatpy
-from pghipy import get_default_window, calculate_synthesis_window
-from pghipy import stft, pghi, istft
 import ctifgan
 import wgan_gp
 import utils
@@ -19,27 +13,10 @@ sample_rate = 16000
 audio_dim = 16384
 clipBelow = -10
 
-def normalize(db_spec):
-    normalized_spec = (db_spec - tf.reduce_min(db_spec)) / (tf.reduce_max(db_spec) - tf.reduce_min(db_spec))
-    return normalized_spec
-
-def denormalize(normalized_spec):
-    denormalized_spec = normalized_spec * 69.374115
-    return denormalized_spec
-
-def generate_spectrograms_and_derivs_from(audio_signals):
-    fft_hop_size = 128
-    fft_window_length = 512
-    L = 16384
-    anStftWrapper = LTFATStft()
-    spectrograms = np.zeros([len(audio_signals), int(fft_window_length//2+1), int(L/fft_hop_size)], dtype=np.float64)
-    # gs = {'name': 'gauss', 'M': 512}
-    
-    for index, audio_signal in enumerate(audio_signals):
-        realDGT = anStftWrapper.oneSidedStft(signal=audio_signal, windowLength=fft_window_length, hopSize=fft_hop_size)
-        spectrogram = anStftWrapper.logMagFromRealDGT(realDGT, clipBelow=np.e**clipBelow, normalize=True)
-        spectrograms[index] = spectrogram  
-    return spectrograms
+def normalize(specs):
+    max_value = tf.reduce_max(specs)
+    normalized_spec = specs / max_value
+    return normalized_spec, max_value
 
 def train_model(n_batches = 10000,
                 batch_size = 128,
@@ -51,18 +28,17 @@ def train_model(n_batches = 10000,
                 synth_frequency = 200,
                 save_frequency = 200,
                 latent_dim = 100,
-                use_batch_norm = False,
                 discriminator_learning_rate = 0.00004,
                 generator_learning_rate = 0.00004,
-                discriminator_extra_steps = 5,
-                phaseshuffle_samples = 0):
+                discriminator_extra_steps = 5):
 
     n_classes = utils.get_n_classes(audio_dir)
     audio, labels = utils.create_dataset(audio_dir, sample_rate, checkpoints_path, audio_size_samples=audio_dim)
 
     wave = tf.reshape(tf.cast(audio,tf.float32),(-1,16384))
     specs = tfio.audio.spectrogram(wave, nfft=254, window=254, stride=64)
-    log_spec = tf.math.log(tf.clip_by_value(t=normalize(specs), clip_value_min=tf.exp(-10.0), clip_value_max=float("inf")))
+    normalized_spec, max_value = normalize(specs)
+    log_spec = tf.math.log(tf.clip_by_value(normalized_spec, clip_value_min=tf.exp(-10.0), clip_value_max=float("inf")))
     log_spec = log_spec/(-clipBelow/2)+1
     print(tf.reduce_min(log_spec), tf.reduce_max(log_spec))
     specs = tf.reshape(log_spec, (-1, 256, 128, 1))
@@ -71,9 +47,7 @@ def train_model(n_batches = 10000,
     #build the discriminator
     discriminator = ctifgan.discriminator(n_classes = n_classes)
     #build the generator
-    generator = ctifgan.generator(z_dim = latent_dim,
-                                                use_batch_norm = use_batch_norm,
-                                                n_classes = n_classes)
+    generator = ctifgan.generator(z_dim = latent_dim, n_classes = n_classes)
     #set the optimizers
     discriminator_optimizer = Adam(learning_rate = discriminator_learning_rate, beta_1=0.5, beta_2=0.9)
     generator_optimizer = Adam(learning_rate = generator_learning_rate, beta_1=0.5, beta_2=0.9)
@@ -94,9 +68,9 @@ def train_model(n_batches = 10000,
     
     #save the training parameters used to the checkpoints folder,
     #it makes it easier to retrieve the parameters/hyperparameters afterwards
-    utils.write_parameters(n_batches, batch_size, audio_path, checkpoints_path, path_to_weights, 
-                           resume_training, override_saved_model, synth_frequency, save_frequency, latent_dim, use_batch_norm, 
-                           discriminator_learning_rate, generator_learning_rate, discriminator_extra_steps,phaseshuffle_samples)
+    utils.write_parameters(n_batches, batch_size, audio_path, checkpoints_path, path_to_weights, max_value,
+                           resume_training, override_saved_model, synth_frequency, save_frequency, latent_dim, 
+                           discriminator_learning_rate, generator_learning_rate, discriminator_extra_steps)
 
     #load the desired weights in path (if resuming training)
     if resume_training == True:
@@ -113,15 +87,8 @@ if __name__ == '__main__':
     audio_dir = '/home/rein/Downloads/allclass/footstep/'
     checkpoints_path = 'footstep_checkpoints/'
 
-    # spectrograms = generate_spectrograms_and_derivs_from(audio)
-    # spectrograms = spectrograms/(-clipBelow/2)+1
-    # specs = spectrograms
-    # specs = np.expand_dims(specs, axis=-1)
-    # specs = specs[:, :256, :]
-    # print(specs.shape, specs.dtype)
-
     train_model(n_batches = 100000,
-                batch_size = 32,
+                batch_size = 16,
                 audio_path = audio_dir,
                 checkpoints_path = checkpoints_path,
                 path_to_weights = 'model_weights.h5',
@@ -130,8 +97,6 @@ if __name__ == '__main__':
                 synth_frequency = 500,
                 save_frequency = 2000,
                 latent_dim = 100,
-                use_batch_norm = True,
                 discriminator_learning_rate = 1e-4,
                 generator_learning_rate = 1e-4,
-                discriminator_extra_steps = 5,
-                phaseshuffle_samples = 0)
+                discriminator_extra_steps = 5)
