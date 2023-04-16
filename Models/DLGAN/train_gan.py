a1e68851663714759309c9b9f51ee4d724acd961
Rein Liu <50271800+Reinliu@users.noreply.github.com>
2023-04-16 16:54:20 +1000

    Update train_gan.py

--- Models/DLGAN/train_gan.py
+++ Models/DLGAN/train_gan.py
@@ -2,20 +2,36 @@
 import ctifgan
 import wgan_gp
 import utils
-from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.keras.optimizers import Adam
 import tensorflow_io as tfio
 import tensorflow as tf
 import numpy as np
-audio_size_samples = 16384
-foldername = 'gstep-2'
+audio_dim = 16384*4
+foldername = 'gstep-1'
 clipBelow = -10
-mse_weight = 1e-4
+mse_weight = 1e-2
 
 def normalize(specs):
     max_value = tf.reduce_max(specs)
     normalized_spec = specs / max_value
     return normalized_spec, max_value
 
+def extract_specs(audio, audio_dim):
+    wave = tf.reshape(tf.cast(audio,tf.float32),(-1,audio_dim))
+    if audio_dim == 16384*4:
+        specs = tfio.audio.spectrogram(wave, nfft=510, window=256, stride=128)
+    elif audio_dim == 16384:
+        specs = tfio.audio.spectrogram(wave, nfft=254, window=256, stride=64)
+    normalized_spec, max_value = normalize(specs)
+    log_spec = tf.math.log(tf.clip_by_value(t=normalized_spec, clip_value_min=tf.exp(-10.0), clip_value_max=float("inf")))
+    log_spec = log_spec/(-clipBelow/2)+1
+    if audio_dim == 16384*4:
+        specs = tf.reshape(log_spec, (-1, 512, 256, 1))
+    elif audio_dim == 16384:
+        specs = tf.reshape(log_spec, (-1, 256, 128, 1))
+    specs = np.asarray(specs)
+
+    return specs, max_value
 
 def train_model(sampling_rate = 16000,
                 n_batches = 10000,
@@ -34,20 +50,13 @@ def train_model(sampling_rate = 16000,
 
     n_classes = utils.get_n_classes(audio_path)
     #create the dataset from the class folders in '/audio'
-    audio, labels = utils.create_dataset(audio_path, sampling_rate, checkpoints_path, audio_size_samples=audio_size_samples)
-    wave = tf.reshape(tf.cast(audio,tf.float32),(-1,16384))
-    specs = tfio.audio.spectrogram(wave, nfft=254, window=256, stride=64)
-    normalized_spec, max_value = normalize(specs)
-    log_spec = tf.math.log(tf.clip_by_value(t=normalized_spec, clip_value_min=tf.exp(-10.0), clip_value_max=float("inf")))
-    log_spec = log_spec/(-clipBelow/2)+1
-    specs = tf.reshape(log_spec, (-1, 256, 128, 1))
-    specs = np.asarray(specs)
-    #print(specs.shape, specs.dtype)
+    audio, labels = utils.create_dataset(audio_path, sampling_rate, checkpoints_path, audio_size_samples=audio_dim)
+    specs, max_value = extract_specs(audio, audio_dim)
 
     #build the discriminator
-    discriminator = ctifgan.discriminator(n_classes = n_classes)
+    discriminator = ctifgan.discriminator(audio_dim = audio_dim, n_classes = n_classes)
     #build the generator
-    generator = ctifgan.generator(latent_dim = latent_dim, n_classes = n_classes)
+    generator = ctifgan.generator(latent_dim = latent_dim, audio_dim = audio_dim, n_classes = n_classes)
     #set the optimizers
     discriminator_optimizer = Adam(learning_rate = discriminator_learning_rate, beta_1=0.5, beta_2=0.9)
     generator_optimizer = Adam(learning_rate = generator_learning_rate, beta_1=0.5, beta_2=0.9)
@@ -79,26 +88,26 @@ def train_model(sampling_rate = 16000,
         gan.load_weights(path_to_weights)
 
     #train the gan for the desired number of batches
-    gan.train(audio = audio, labels = labels, specs=specs, batch_size = batch_size, batches = n_batches, 
+    gan.train(audio = audio, labels = labels, specs=specs, audio_dim = audio_dim, batch_size = batch_size, batches = n_batches, 
                  max_value = max_value, synth_frequency = synth_frequency, save_frequency = save_frequency,
                  checkpoints_path = checkpoints_path, override_saved_model = override_saved_model,
                  sampling_rate = sampling_rate, n_classes = n_classes, mse_weight = mse_weight)
 
 
 if __name__ == '__main__':
-    audio_dir = '/home/rein/Downloads/allclass/footstep/'
-    checkpoints_path = 'footstep_checkpoints/'
+    audio_dir = '/home/rein/Downloads/development-dataset/raining/'
+    checkpoints_path = 'challenge_checkpoints/'
 
     train_model(n_batches = 100000,
                 sampling_rate = 16000,
-                batch_size = 16,
+                batch_size = 4,
                 audio_path = audio_dir,
                 checkpoints_path = checkpoints_path,
                 path_to_weights = 'model_weights.h5',
                 resume_training = False,
                 override_saved_model = True,
-                synth_frequency = 5000,
-                save_frequency = 10000,
+                synth_frequency = 2000,
+                save_frequency = 2000,
                 latent_dim = 100,
                 discriminator_learning_rate = 1e-4,
                 generator_learning_rate = 1e-4,
